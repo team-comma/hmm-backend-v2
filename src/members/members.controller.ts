4b6aaca267c2be737744051bc3d2dd976d0771d3
김대욱 <daeuk.kim@dbdlab.io>
2022-12-24 21:40:00 +0900

    feat: 회원 정보 업데이트 기능 추가

--- src/members/members.controller.ts
+++ src/members/members.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Put, UseGuards } from '@nestjs/common';
+import { UpdateMemberDto } from './dto/update-member.dto';
+import { MembersService } from './members.service';
 
 @Controller('members')
-export class MembersController {}
+export class MembersController {
+  constructor(private readonly membersService: MembersService) {}
+
+  @Put('profile/:id')
+  public updateMember(@Param('id') memberId: string, @Body() updateMemberDto: UpdateMemberDto) {
+    return this.membersService.updateMemberById(memberId, updateMemberDto);
+  }
+}
